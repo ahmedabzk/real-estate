ef97a37861823f7a95a7583f0f2cd27a163052fd
Yasir <goodwonder5@gmail.com>
2023-11-28 21:21:58 +0530

    Fix response

--- backend/src/controllers/user_controller.rs
+++ backend/src/controllers/user_controller.rs
@@ -1,10 +1,9 @@
-use axum::extract::{State, Json};
+use axum::extract::{Json, State};
 
 use axum::http::{HeaderMap, StatusCode};
+use axum::response::{IntoResponse, Response};
 use chrono::Utc;
 use mongodb::bson::doc;
-use axum::response::{IntoResponse, Response};
-
 
 use axum::debug_handler;
 use mongodb::Client;
@@ -64,9 +63,11 @@ pub async fn login(
     State(client): State<Client>,
     State(secret): State<TokenWrapper>,
     Json(payload): Json<UserLogin>,
-) -> Result<Json<UserResponse>, MyError>{
+) -> impl IntoResponse {
     if payload.username.is_empty() || payload.password.is_empty() {
-        return Err(MyError::MissingCredential("missing credentials".to_string()));
+        return Err(MyError::MissingCredential(
+            "missing credentials".to_string(),
+        ));
     }
 
     let mut headers = HeaderMap::new();
@@ -77,38 +78,38 @@ pub async fn login(
         .find_one(doc! {"username": payload.username}, None)
         .await?;
 
-    if let Some(user) = user{
+    if let Some(user) = user {
         let password_verification = verify_password(&payload.password, &user.password).await?;
-        if !password_verification{
-            return Err(MyError::WrongCredential("check your password and username".to_string()));
-        }else{
+        if !password_verification {
+            Err(MyError::WrongCredential(
+                "check your password and username".to_string(),
+            ))
+        } else {
             let token = generate_token(secret.0, user.id.to_string()).await;
             let cookie_name = "auth-cookie".to_string();
             let cookie = format!("{}={} HttpOnly", cookie_name, token);
             headers.insert(
                 axum::http::header::SET_COOKIE,
-                cookie.as_str().parse().unwrap()
+                cookie.as_str().parse().unwrap(),
             );
 
-            let res = UserResponse{
+            let res = UserResponse {
                 status: "success",
                 id: user.id.to_string(),
                 username: user.username,
                 // token: cookie
             };
 
-             let response = Response::builder()
-             .status(StatusCode::OK)
-            .header(axum::http::header::SET_COOKIE,cookie.as_str())
-            .body(Json(res)).unwrap().into_body();
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header(axum::http::header::SET_COOKIE, cookie.as_str())
+                .body(Json(res))
+                .unwrap()
+                .into_body();
 
-            //   Ok(response.into())
-
-            Ok(response.into_response())
+            Ok(response)
         }
-    }else{
-        return Err(MyError::UserDoesNotExist("User does not exist".to_string()));
+    } else {
+        Err(MyError::UserDoesNotExist("User does not exist".to_string()))
     }
 }
-
-
